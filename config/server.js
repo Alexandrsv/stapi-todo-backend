e6588435e4a64c799084be84d881380510e06b71
alexandr <zbc@mail.ru>
2022-02-07 23:59:00 +0300

    rename strapi port env variable

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
-const cronTasks = require("./cron-tasks");
+// const cronTasks = require("./cron-tasks");
 
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('NODE_PORT', 1337),
   // cron: {
   //   enabled: true,
   //   tasks: cronTasks,
